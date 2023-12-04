92ca3f51651e675583feec0060d9f8ecf3fa54a1
Karlen95 <karlen.h.1995@gmail.com>
2023-12-04 21:15:30 +0400

    lesson 3 node js

--- GameOfLife/index.js
+++ GameOfLife/index.js
@@ -1,51 +1,16 @@
-// let arr = [1,3,5,"tox", true, [3,4,5,]];
+let express = require("express");
+let app = express();
+
+app.get("/", function(req, res){
+   res.send("<h1>Hello world</h1>");
+});
+
+app.get("/anun/azganun/:anun", function(req, res){
+   let name = req.params.anun;
+   res.send("<h1>Hello " + name +"</h1>");
+});
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
 
 
- 
-
-// let usanox = {
-//     anun:"Arsen",
-//     azganun:"Arsenyan",
-//     tarig:17,
-//  displayInfo(){
-//      console.log(this.anun);
-//  }
-// }
-
-
-// class Usanox{
-//     constructor(anun, azganun, tarig){
-//         this.anun = anun;
-//         this.azganun = azganun;
-//         this.tarig = tarig;
-//         this.id= "898989"
-//     }
-
-//     info(){
-//         console.log(this.anun,this.azganun, this.id);
-//     }
-// }
-
-// let usanox1 = new Usanox("Mane", "Parsamyan", 13)
-// usanox1.info()
-
-// console.log(JSON.stringify(usanox1));
-
-// let objUsanox = JSON.stringify(usanox)
-// objUsanox = JSON.parse(objUsanox)
-
-// let tiv = 6;
-
-// console.log(typeof usanox);
-// console.log(typeof objUsanox);
-// console.log([1,2,3,4,5]);
-
-
-
-// let axavni = new Axavni("chalubalak", "spitak", "bmblot", "karch");
-// console.log(axavni);
-// axavni.trnel()
-
-
-let arciv = new Arciv("Kovkasyan", "erkar", "sur", "erkar u sur", true);
-arciv.trnel()
